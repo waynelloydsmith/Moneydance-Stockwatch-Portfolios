d6ad2fdb6d6a8218e8df570b78b6f87323a38b2c
waynelloydsmith <37414657+waynelloydsmith@users.noreply.github.com>
2019-05-11 13:24:03 -0600

    Add files via upload

---
+++ updatePortfolioStockwatch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# called by RunSripts.py
+execfile("updatePortfolio1Stockwatch.py")
+execfile("updatePortfolio2Stockwatch.py")
+execfile("updatePortfolio3Stockwatch.py")
+execfile("updatePortfolio4Stockwatch.py")
+print "Done updatePortfolioStockwatch.py"
